2f5ee373d9f5cb6254a94ac88ff7b48b76722609
Riley Moore <rilemoore@gmail.com>
2019-09-15 23:39:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,4 @@
-status = "Thank You Mario! But Our Princess Is In Another Castle!"
+
 
 phrase = "Do a Barrel Roll!"
 
@@ -8,6 +8,7 @@ def mario
 end
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
